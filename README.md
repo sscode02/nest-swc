c423348e4fa29c7b7c588fe7931cc8bf6ae7764f
joslynyang <joslynyang@tencent.com>
2022-03-11 13:53:01 +0800

    feat: addNestProject

---
+++ README.md
@@ -0,0 +1,11 @@
+```
+# Test
+
+```bash
+# e2e tests ts-jest
+$ npm run test:e2e
+
+# e2e tests swc/jest
+$ npm run test:e2e:swc
+
+
---
+++ jest-e2e.js
@@ -0,0 +1,29 @@
+module.exports = {
+  rootDir: './',
+  testEnvironment: 'node',
+  transform: {
+    "^.+\\.(t|j)sx?$": "ts-jest",
+  },
+  testMatch: ['<rootDir>/test/*.(spec|test).ts'],
+  testPathIgnorePatterns: ['/node_modules/'],
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  moduleNameMapper: {
+    '^src/(.*)$': '<rootDir>/src/$1',
+  },
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.(t|j)s',
+    '!<rootDir>/src/main.ts',       
+    '!<rootDir>/**/*.module.ts',    
+  ],
+  collectCoverage: true,
+  coverageDirectory: '<rootDir>/coverage',
+  coverageThreshold: {
+    global: {
+      statements: 100,
+      branches: 100,
+      functions: 100,
+      lines: 100,
+    },
+  },
+  reporters: ['default', 'jest-html-reporters'],
+}
